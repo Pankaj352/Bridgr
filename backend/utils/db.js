db833803ac16b89a6b78998ac4704d1b32a1dcd5
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-03-12 23:46:31 +0530

    commit changes

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('mongodb connected successfully.');
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      authMechanism: "SCRAM-SHA-1", // This line forces the use of SCRAM-SHA-1 mechanism
+    });
+    console.log("MongoDB connected successfully.");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error);
+  }
+};
+
 export default connectDB;
