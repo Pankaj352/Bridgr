d1241a64aca6141b4a578c65be934498517e3674
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-03-13 00:05:26 +0530

    commit changes

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      authMechanism: "SCRAM-SHA-1", // This line forces the use of SCRAM-SHA-1 mechanism
     });
-    console.log("MongoDB connected successfully.");
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("Failed to connect to MongoDB:", error);
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
 };
 
