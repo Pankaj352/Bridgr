66ef1677792c42dd3a5aa2514014f1ab54500216
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-03-14 00:01:39 +0530

    commit changes

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -65,9 +65,8 @@ const Profile = () => {
               <div className='flex flex-col gap-1'>
                 <span className='font-semibold'>{userProfile?.bio || 'bio here...'}</span>
                 <Badge className='w-fit' variant='secondary'><AtSign /> <span className='pl-1'>{userProfile?.username}</span> </Badge>
-                <span>🤯Learn code with patel mernstack style</span>
-                <span>🤯Turing code into fun</span>
-                <span>🤯DM for collaboration</span>
+                <span>Awesome ✌️</span>
+                
               </div>
             </div>
           </section>
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -63,7 +63,7 @@ const Signup = () => {
         onSubmit={signupHandler}
         className="shadow-lg flex flex-col gap-5 p-8">
         <div className="my-4">
-          <h1 className="text-center font-bold text-xl">LOGO</h1>
+          <h1 className="text-center font-bold text-xl">Bridgr</h1>
           <p className="text-sm text-center">
             Signup to see photos & videos from your friends
           </p>
