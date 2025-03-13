02724c60d70ac274d3c21bc8ef629f82b2a050fb
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-03-14 00:25:38 +0530

    commit changes

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 
 const corsOptions = {
-    origin: process.env.URL,
+    origin:process.env.URL,
     credentials: true
 }
 app.use(cors(corsOptions));
