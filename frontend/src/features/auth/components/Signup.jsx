495d12d06f86121dad25f6873894647fb2be9e8e
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-03-15 01:38:39 +0530

    commit changes

--- frontend/src/features/auth/components/Signup.jsx
+++ frontend/src/features/auth/components/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { toast } from "sonner";
 import { Link, useNavigate } from "react-router-dom";
