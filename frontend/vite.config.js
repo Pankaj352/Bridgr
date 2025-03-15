9cff886bccf4b780472558def2705f75f03d7593
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-03-15 15:10:58 +0530

    commit changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,14 +11,15 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['date-fns', '@emoji-mart/data', '@emoji-mart/react', 'redux-persist/integration/react', 'next-themes'],
+      external: ['date-fns', '@emoji-mart/data', '@emoji-mart/react', 'redux-persist/integration/react', 'next-themes', '@radix-ui/react-popover'],
       output: {
         globals: {
           'date-fns': 'dateFns',
           '@emoji-mart/data': 'emojiMartData',
           '@emoji-mart/react': 'emojiMartReact',
           'redux-persist/integration/react': 'ReduxPersistIntegration',
-          'next-themes': 'nextThemes'
+          'next-themes': 'nextThemes',
+          '@radix-ui/react-popover': 'RadixUIReactPopover'
         }
       }
     }
